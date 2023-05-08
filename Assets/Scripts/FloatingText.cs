391f4028dbb3cfe2f8e586281031de7547c80b72
Rhys Webb <rtaddley@gmail.com>
2023-05-09 08:00:19 +1200

    changes

--- Assets/Scripts/FloatingText.cs
+++ Assets/Scripts/FloatingText.cs
@@ -6,14 +6,20 @@ public class FloatingText : MonoBehaviour
 {
     // Variables --------------------------------------------
     [SerializeField] private float speed;
+    [SerializeField] private float textSpeed;
     // Variables --------------------------------------------
 
-    
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
 
     private void Update()
     {
-        transform.Translate(new Vector3(0,1,0) * Time.deltaTime * speed);    
-    }
+        transform.Translate(new Vector3(0,1,0) * Time.deltaTime * speed);
+        if (transform.localScale >= Vector3.one)
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * Time.deltaTime * textSpeed;
 
+    }
 
 }
