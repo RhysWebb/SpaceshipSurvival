77c5c7c1a5948a7598c9b4c2c95eea0935183619
Rhys Webb <rtaddley@gmail.com>
2023-05-10 20:04:24 +1200

    polishing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     // Variables ---------------------------------------------
     // Speed -------------------------------------------------
-    [SerializeField] private float speed = 1.2f;
+    [SerializeField] private float speed = 4f;
     private float maxVelocity = 1.2f;
     private float rotationalSpeed = 125f;
     // Speed -------------------------------------------------
